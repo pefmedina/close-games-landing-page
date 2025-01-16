cfc5fba034f5c51e7ef4cb63dd72a04bfa7b324e
Pedro <103079315+pefmedina@users.noreply.github.com>
2025-01-16 14:42:25 -0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2390133591"
+    serviceWorkerVersion: "2653827290"
   }
 });
